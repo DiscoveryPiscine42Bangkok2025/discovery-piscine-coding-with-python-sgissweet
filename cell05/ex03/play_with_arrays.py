4c5bcabad12d8c12dfe9276a40b36a779c8ecefb
sgissweet <nitkamon.67908@gmail.com>
2025-09-18 00:00:44 +0700

    cell05

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ori_arr = [2, 8, 9, 48, 8, 22, -12, 2]
-new_arr = list(dict.fromkeys([x + 2 for x in ori_arr if x > 5]))
+new_arr = set([i + 2 for i in ori_arr if i > 5])
 
 print(ori_arr)
 print(new_arr)
