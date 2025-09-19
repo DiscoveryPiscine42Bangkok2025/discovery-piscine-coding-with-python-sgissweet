2e8f7050f9e24377f3debb5cadea68f6a292f90b
pang_loves_molamola <72025305+sgissweet@users.noreply.github.com>
2025-09-19 14:00:44 +0700

    Update string_are_arrays.py

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -7,8 +7,9 @@ if len(sys.argv) == 2:
     for char in sys.argv[1]:
         if char == 'z':
             z_txt += 'z'
-    print(z_txt)
     if z_txt == "":
         print("none")
+    else:
+        print(z_txt)
 else:
     print("none")
